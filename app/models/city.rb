3807de2462d5d9ecaff0d4330ffa95586b3fa7c6
Stephen Crane <scrane88@gmail.com>
2021-09-23 09:23:55 -0400

    refactor date overlap functionality

--- app/models/city.rb
+++ app/models/city.rb
@@ -4,10 +4,10 @@ class City < ActiveRecord::Base
   has_many :reservations, through: :listings
 
   def city_openings(start_date, end_date)
-    query_datetimes = { start_a: start_date.to_datetime, end_a: end_date.to_datetime }
+    query_datetimes = { start_date: start_date.to_datetime, end_date: end_date.to_datetime }
     listings.filter do |listing|
       listing.reservations.none? do |reservation|
-        dates_overlap?({ start_b: reservation.checkin, end_b: reservation.checkout }.merge(query_datetimes))
+        reservation.dates_overlap?(query_datetimes)
       end
     end
   end
@@ -18,12 +18,6 @@ class City < ActiveRecord::Base
 
   def self.most_res
     all.max_by { |city| city.reservations.count }
-  end
-
-private
-
-  def dates_overlap?(start_a:, end_a:, start_b:, end_b:)
-    [start_a, start_b].max < [end_a, end_b].min
   end
 end
 
