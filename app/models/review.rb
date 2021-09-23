069097c58f47d39d24016401ec7564f6245c2af9
Stephen Crane <scrane88@gmail.com>
2021-09-23 15:55:56 -0400

    Review spec passing

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,13 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, presence: true
+  validates :description, presence: true
+  validate :valid_past_reservation
+
+  def valid_past_reservation
+    if !reservation || reservation.status != "accepted" || reservation.checkout > Time.now
+      errors.add(:reservation, "is not accepted, or is still in progress")
+    end
+  end
 end
